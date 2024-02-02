3f956787afba026377a65976cc07451043d9556f
LxCoding <2176427177@qq.com>
2024-02-02 21:19:56 +0800

    添加节点、二叉树模版代码

---
+++ BinNode.hpp
@@ -0,0 +1,163 @@
+#include"..\..\Stack\Stack\template-of-Stack\Stack.hpp"
+#include"..\..\Queue\Queue\template-of-Queue\Queue.hpp"
+#include<iostream>
+using namespace std;
+
+#define BinNodePosi(T) BinNode<T>*
+#define stature(p) ((p) ? (p)->height : -1)
+#define IsRoot(x) (!((x).parent))
+#define IsLChild(x) (!IsRoot(x) && (& (x) == (x).parent->lc))
+#define IsRChild(x) (!IsRoot(x) && (& (x) == (x).parent->rc))
+#define HasParent(x) (!IsRoot(x))
+#define HasLChild(x) ((x).lc)
+#define HasRChild(x) ((x).rc)
+#define HasChild(x) (HasLChild(x) || HasRChild(x))
+#define HasBothChild(x) (HasLChild(x) && HasRChild(x))
+#define IsLeaf(x) (!HasChild(x))
+#define sibling(p) (IsLChild(*(p)) ? (p)->parent->rc : (p)->parent->lc)
+#define uncle(p) (IsLChild(*((x)->parent)) ? (x)->parent->parent->rc : (x)->parent->parent->lc)
+#define FromParentTo(x) (IsRoot(x) ? this->_root : (IsLChild(x) ? (x).parent->lc : (x).parent->rc))
+typedef enum { RED, BLACK } RBColor;
+
+template <typename T> struct BinNode {
+	T data;
+	BinNodePosi(T) parent;
+	BinNodePosi(T) lc;
+	BinNodePosi(T) rc;
+	int height;
+	int npl;
+	RBColor color;
+
+	BinNode() :
+		parent(NULL), lc(NULL), rc(NULL), height(0), npl(1), color(RED) {}
+	BinNode(T e, BinNodePosi(T) p = NULL, BinNodePosi(T) lc = NULL, BinNodePosi(T) rc = NULL,
+		int h = 0, int l = 1, RBColor c = RED) :
+		data(e), parent(p), lc(lc), rc(rc), height(h), npl(l), color(c) {}
+	BinNodePosi(T) insertAsLC(T const& e);
+	BinNodePosi(T) insertAsRC(T const& e);
+	BinNodePosi(T) succ();
+	void travLevel();
+	void travPre();
+	void travIn();
+	void travPost();
+
+	bool operator < (BinNode const& bn) { return data < bn.data; }
+	bool operator > (BinNode const& bn) { return data > bn.data; }
+	bool operator == (BinNode const& bn) { return data == bn.data; }
+};
+
+template <typename T> BinNodePosi(T) BinNode<T>::insertAsLC(T const& e)
+{
+	return this->lc = new BinNode(e, this);
+}
+
+template <typename T> BinNodePosi(T) BinNode<T>::insertAsRC(T const& e)
+{
+	return this->rc = new BinNode(e, this);
+}
+
+template<typename T>
+static void visitAlongVine(BinNodePosi(T) x, Stack<BinNodePosi(T)>& S) {
+	while (x) {
+		cout << x->data << ' ' << x->color << endl;
+		S.push(x->rc);
+		x = x->lc;
+	}
+}
+
+template<typename T>
+void BinNode<T>::travPre() {
+	Stack<BinNodePosi(T)> S;
+	BinNodePosi(T) x = this;
+	while (true)
+	{
+		visitAlongVine(x, S);
+		if (S.empty()) break;
+		x = S.pop();
+	}
+}
+
+template<typename T>
+static void goAlongVine(BinNodePosi(T) x, Stack<BinNodePosi(T)>& S) {
+	while (x) {
+		S.push(x);
+		x = x->lc;
+	}
+}
+
+template<typename T>
+void BinNode<T>::travIn() {
+	Stack<BinNodePosi(T)> S;
+	BinNodePosi(T) x = this;
+	while (true)
+	{
+		goAlongVine(x, S);
+		if (S.empty()) break;
+		x = S.pop();
+		cout << x->data << endl;
+		x = x->rc;
+	}
+}
+
+template <typename T> BinNodePosi(T) BinNode<T>::succ()
+{
+	BinNodePosi(T) s = this;
+	if (rc) {
+		s = rc;
+		while (HasLChild(*s))
+			s = s->lc;
+	}
+	else {
+		while (IsRChild(*s))
+			s = s->parent;
+	}
+	return s;
+}
+
+template<typename T>
+static void gotoLeft(Stack<BinNodePosi(T)>& S) {
+	while (BinNodePosi(T) x = S.top()) {
+		if (HasLChild(*x)) {
+			if (HasRChild(*x))
+				S.push(x->rc);
+			S.push(x->lc);
+		}
+		else
+			S.push(x->rc);
+	}
+	S.pop();
+}
+
+template<typename T>
+void BinNode<T>::travPost() {
+	Stack<BinNodePosi(T)> S;
+	BinNodePosi(T) x = this;
+	if (x) S.push(x);
+	while (!S.empty())
+	{
+		if (S.top() != x->parent)
+			gotoLeft(S);
+		x = S.pop();
+		cout << x->data << endl;
+	}
+}
+
+template<typename T>
+void BinNode<T>::travLevel() {
+	Queue<BinNodePosi(T)> Q;
+	Q.enqueue(this);
+	while (!Q.empty())
+	{
+		BinNodePosi(T) x = Q.dequeue();
+		cout << x->data << endl;
+		if (HasLChild(*x))
+			Q.enqueue(x->lc);
+		if (HasRChild(*x))
+			Q.enqueue(x->rc);
+	}
+}
+
+
+
+
+
---
+++ BinTree.hpp
@@ -0,0 +1,96 @@
+#include"BinNode.hpp"
+#include<algorithm>
+#include<memory>
+
+template <typename T> class BinTree {
+protected:
+	int _size;
+	BinNodePosi(T) _root;
+	virtual int updateHeight(BinNodePosi(T) x);
+	void updateHeightAbove(BinNodePosi(T) x);
+public:
+	BinTree() : _size(0), _root(NULL) {}
+	~BinTree() { if (0 < _size) remove(_root); }
+	int size() const { return _size; }
+	bool empty() const { return !_root; }
+	BinNodePosi(T) root() const { return _root; }
+	BinNodePosi(T) insertAsRoot(T const& e);
+	BinNodePosi(T) insert(T const& e, BinNodePosi(T) x);
+	BinNodePosi(T) insert(BinNodePosi(T) x, T const& e);
+	BinNodePosi(T) attach(BinTree<T>* &S, BinNodePosi(T) x);
+	BinNodePosi(T) attach(BinNodePosi(T) x, BinTree<T>* &S);
+	void travLevel() { if (_root) _root->travLevel(); }
+	void travPre() { if (_root) _root->travPre(); }
+	void travIn() { if (_root) _root->travIn(); }
+	void travPost() { if (_root) _root->travPost(); }
+	int remove(BinNodePosi(T) x);
+};
+
+template <typename T> int BinTree<T>::updateHeight(BinNodePosi(T) x)
+{
+	return x->height = 1 + max(stature(x->lc), stature(x->rc));
+}
+
+template <typename T> void BinTree<T>::updateHeightAbove(BinNodePosi(T) x)
+{
+	while (x) { updateHeight(x); x = x->parent; }
+}
+
+template <typename T> BinNodePosi(T) BinTree<T>::insertAsRoot(T const& e) {
+	_size = 1;
+	return _root = new BinNode<T>(e);
+}
+
+template <typename T> BinNodePosi(T) BinTree<T>::insert(T const& e, BinNodePosi(T) x) {
+	_size++;
+	x->insertAsLC(e);
+	updateHeightAbove(x);
+	return x->lc;
+}
+
+template <typename T> BinNodePosi(T) BinTree<T>::insert(BinNodePosi(T) x, T const& e) {
+	_size++;
+	x->insertAsRC(e);
+	updateHeightAbove(x);
+	return x->rc;
+}
+
+template <typename T> BinNodePosi(T) BinTree<T>::attach(BinTree<T>* &S, BinNodePosi(T) x) {
+	if (x->lc = S->_root) x->lc->parent = x;
+	_size += S->_size;
+	updateHeightAbove(x);
+	S->_root = NULL;
+	S->_size = 0;
+	release(S);
+	S = NULL;
+	return x;
+}
+
+template <typename T> BinNodePosi(T) BinTree<T>::attach(BinNodePosi(T) x, BinTree<T>* &S) {
+	if (x->rc = S->_root) x->rc->parent = x;
+	_size += S->_size;
+	updateHeightAbove(x);
+	S->_root = NULL;
+	S->_size = 0;
+	delete S;
+	S = NULL;
+	return x;
+}
+
+template <typename T> 
+static int removeAt(BinNodePosi(T) x) {
+	if (!x) return 0;
+	int n = 1 + removeAt(x->lc) + removeAt(x->rc);
+	delete x;
+	return n;
+}
+
+template <typename T>
+int BinTree<T>::remove(BinNodePosi(T) x) {
+	FromParentTo(*x) = NULL;
+	updateHeightAbove(x->parent);
+	int n = removeAt(x);
+	_size -= n;
+	return n;
+}
+
